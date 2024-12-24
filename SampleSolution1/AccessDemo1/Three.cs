4da50b898685c529c11c3f93c4aee9205cd41ffe
Rohithgowdakh <rohithkh2020@gmail.com>
2024-12-24 11:19:15 +0530

    Access Specifiers in Csharp

---
+++ SampleSolution1/AccessDemo1/Three.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessDemo1
+{
+    internal class Three
+    {
+        static void Main()
+        {
+            Program p=new Program();
+            // p.Test1(); private only same class
+           // p.Test3(); protected subclass only in another class
+            p.Test2();
+            p.Test4();
+            p.Test5();
+            
+        }
+    }
+}
---
+++ SampleSolution1/AccessDemo1/Two.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessDemo1
+{
+    internal class Two : Program
+    {
+        static void Main(string[] args)
+        {
+            Two two = new Two();
+           // two.Test1(); private members can be accessible only within same class
+            two.Test2();
+            two.Test3();
+            two.Test4();
+            two.Test5();
+            
+        }
+    }
+}
