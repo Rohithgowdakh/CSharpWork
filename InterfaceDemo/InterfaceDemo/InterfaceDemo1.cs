b2009dc5151ef0dbf7ba7d2dfa2e1618688ddd80
Rohithgowdakh <rohithkh2020@gmail.com>
2024-12-26 15:02:46 +0530

    Interfaces using Csharp

---
+++ InterfaceDemo/InterfaceDemo/InterfaceDemo1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace InterfaceDemo
+{
+    interface InterfaceTest1
+    {
+        void Test();
+    }
+    interface InterfaceTest2
+    {
+        void Test();
+    }
+    public abstract class TestClass
+    {
+        public abstract void Test1();
+    }
+
+    class InterfaceImplement : TestClass, InterfaceTest1, InterfaceTest2 
+    {
+        public void Test()
+        {
+            Console.WriteLine(" Multiple Interface  Test Method ");
+        }
+        void InterfaceTest1.Test()
+        {
+            Console.WriteLine("Interface 1 Test Method ");
+
+        }
+
+        void InterfaceTest2.Test()
+        {
+            Console.WriteLine("Interface 2 Test Method");
+        }
+        public override void Test1()
+        {
+            Console.WriteLine("Abstract Class Test Method");
+        }
+        static void Main(string[] args)
+        {
+            InterfaceImplement ii=new InterfaceImplement();
+            ii.Test();
+            ii.Test1 ();
+            InterfaceTest1 i1 = ii;
+            i1.Test();
+            InterfaceTest2 i2 = ii;
+            i2.Test();
+        }
+
+       
+    }
+}
