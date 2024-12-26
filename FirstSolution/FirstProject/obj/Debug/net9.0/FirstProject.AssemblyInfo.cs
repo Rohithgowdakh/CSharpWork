15edc9f86dac32726746b4f2a0526379874fde25
Rohithgowdakh <rohithkh2020@gmail.com>
2024-12-26 11:24:37 +0530

    Constructor

--- FirstSolution/FirstProject/obj/Debug/net9.0/FirstProject.AssemblyInfo.cs
+++ FirstSolution/FirstProject/obj/Debug/net9.0/FirstProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da50b898685c529c11c3f93c4aee9205cd41ffe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e0b9c2f9fa73484a0c9164a3e7494bd3a837e29")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
