316954c9bcf74b297a006c8f91f5a846b1f19912
Rohithgowdakh <rohithkh2020@gmail.com>
2024-12-27 11:03:34 +0530

    Structures in Csharp

---
+++ StructureDemo/StructureDemo/CodeFile1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StructureDemo
+{
+    struct StructDemo
+    {
+        public int x;
+        public StructDemo()
+        {
+
+            Console.WriteLine("Implicit constructor  in structure");
+        }
+        public StructDemo(int x)
+        {
+            this.x = x;
+            Console.WriteLine("parameterized constructor  in structure");
+        }
+
+        public void Test()
+        {
+            Console.WriteLine("Structure Test Method");
+        }
+        static void Main()
+        {
+            StructDemo sd;
+            sd.x = 5;
+            sd.Test();
+            StructDemo sd3 = new StructDemo();
+            sd3.Test();
+            StructDemo sd2 = new StructDemo(10);
+            sd2.Test();
+        }
+    }
+}
