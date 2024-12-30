1784e8d31157b9d90ead3d62efd4aaf5bd78cb1c
Rohithgowdakh <rohithkh2020@gmail.com>
2024-12-30 13:17:03 +0530

    Indexers in Csharp

---
+++ IndexersDemo/IndexersDemo/EmployeeIndexerInt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace IndexersDemo
+{
+    class Employee
+    {
+        int _EmpId;
+        string _Name;
+        string _Dname;
+        double _Salary;
+        string _Location;
+
+        public Employee(int empId, string name, string dname, double salary, string location)
+        {
+            _EmpId = empId;
+            _Name = name;
+            _Dname = dname;
+            _Salary = salary;
+            _Location = location;
+        }
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return _EmpId;
+                else if (index == 1) return _Name;
+                else if (index == 2) return _Dname;
+                else if (index == 3) return _Salary;
+                else if (index == 4) return _Location;
+                else return 0;
+            }
+            set
+            {
+                if (index == 0) _EmpId = (int)value;
+                else if (index == 1) _Name = (string)value;
+                else if (index == 2) _Dname = (string)value;
+                else if (index == 3) _Salary = (double)value;
+                else if (index == 4) _Location = (string)value;
+                else throw new ArgumentException("This Index not available");
+
+            }
+        }
+    }
+}
---
+++ IndexersDemo/IndexersDemo/EmployeeIndexersStringDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersDemo
+{
+    public class EmployeeIndexersStringDetails
+    {
+        static void Main(string[] args)
+        {
+            EmployeeIndexerString emp = new EmployeeIndexerString(101, "Manu", "Sales", 50000.00, "Bengaluru");
+            Console.WriteLine("Employee Id :" + emp["eid"]);
+            Console.WriteLine("Employee Name :" + emp["name"]);
+            Console.WriteLine("Department Name: " + emp["dname"]);
+            Console.WriteLine("Employeee Salary :" + emp["salary"]);
+            Console.WriteLine("Employee Location :" + emp["location"]);
+
+
+            Console.WriteLine();
+            emp["name"] += " D P ";
+            emp["dname"] = "Data Science";
+            Console.WriteLine("Employee Id :" + emp["eid"]);
+            Console.WriteLine("Employee Name :" + emp["name"]);
+            Console.WriteLine("Department Name: " + emp["dname"]);
+            Console.WriteLine("Employeee Salary :" + emp["salary"]);
+            Console.WriteLine("Employee Location :" + emp["location"]);
+        }
+    }
+}
