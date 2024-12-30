bce47888759d6be4ffc8dde3871d2aaf04f50d16
Rohithgowdakh <rohithkh2020@gmail.com>
2024-12-30 13:18:26 +0530

    Delegates, Asynchrouns Methods , Lambda Expressions

---
+++ DelrgatesDemo/DelrgatesDemo/AnonymousFunc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelrgatesDemo
+{
+    public delegate string HelloDelegate(string message);
+    public class AnonymousFunc
+    {
+
+        public static string SayHello(string name) {
+            return "Hello " + name + " Good Morning !..";
+        }
+        static void Main(string[] args)
+        {
+            HelloDelegate dd = SayHello;
+            string s1= dd.Invoke("Manu");
+            Console.WriteLine(s1);
+
+            //Asynchronous 
+            HelloDelegate d2 = delegate (string name)
+            {
+                return "Hello " + name + " Good Morning !..";
+            };
+            string s2=d2.Invoke("Manu");
+            Console.WriteLine(s2);
+        }
+    }
+}
---
+++ DelrgatesDemo/DelrgatesDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace DelegatesDemo
+{
+    public delegate void AddDigit(int a, int b);
+    public delegate string SayHi(string s);
+    class Program
+    {
+        public void AddNums(int a, int b)
+        {
+            Console.WriteLine(a +  b);
+        }
+        public static string SayHello(string name)
+        {
+            return "Hello " + name;
+        }
+        public static void Main(string[] args)
+        {
+            Program p=new Program();
+            AddDigit an = new AddDigit(p.AddNums);
+            SayHi sh = new SayHi(SayHello);
+            string s = sh("Manu");
+            Console.WriteLine(s);
+            an(100, 200);
+            an.Invoke(100, 100);
+            string ss = sh.Invoke("Sai");
+            Console.WriteLine(ss);
+        }
+    }
+}
