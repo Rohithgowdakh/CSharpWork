ce818b6ba7e5aa0ee250da4be0b95e74badf2a70
Rohithgowdakh <rohithkh2020@gmail.com>
2025-01-07 18:33:15 +0530

    Sample Travel Ticket Booking Console Appication

---
+++ TravelTicketBooking/TravelTicketBooking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TravelTicketBooking
+{
+    public class Travel
+    {
+        public static void Main(string[] args)
+        {
+            TravelUtility book = new TravelUtility();
+            book.LoginPage();
+        }
+    }
+}
