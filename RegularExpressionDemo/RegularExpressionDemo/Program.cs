8ded643afd8dc8849c9b2659135509f162862d48
Rohithgowdakh <rohithkh2020@gmail.com>
2025-01-07 18:33:55 +0530

    Regular Expression Demo

---
+++ RegularExpressionDemo/RegularExpressionDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace RegularExpressionDemo
+
+{
+    public class RegexDemo
+    {
+        public static void RegexForEmail(string email)
+        {
+            string pattern = @"^[A-Za-z0-9.%&_+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(email);
+            bool bb = regex.IsMatch(email);
+            Console.WriteLine(bb);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+        public static void RegexForNumber(long number)
+        {
+            string pattern = @"^\+?[1-9]\d{0,2}?[.-\s]?\(?\d{1-4}\)?[.-\s]?\d{1,4}[-.\s]?\d{1-9}$";
+            Regex regex = new Regex(pattern);
+            
+        }
+        static void Main(string[] args)
+        {
+            string email = "rohithkh2024@gmail.com";
+            RegexForEmail(email);
+        }
+    }
+}
